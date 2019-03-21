4c51a6384a1ea9a66144a53058dac0454499ca75
jambari <jambari07@gmail.com>
2019-03-21 14:02:05 +0900

    update detail earthquake page

--- resources/views/gempa/detail_gempa.blade.php
+++ resources/views/gempa/detail_gempa.blade.php
@@ -1,10 +1,7 @@
-@extends('main')
-@section('title')
+@extends('main') @section('title')
 <title>detail Berita - Stasiun Geofisika Kelas I Angkasapura Jayapura</title>
 @section('after_style')
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
-integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
-crossorigin=""/>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin="" />
 <style type="text/css" media="screen">
     #streetmap {
         display: flex;
@@ -12,206 +9,204 @@ crossorigin=""/>
         align-items: center;
         flex-direction: column;
     }
-    .css-icon {
-
-        }
-
-    .gps_ring {	
+    
+    .css-icon {}
+    
+    .gps_ring {
         border: 3px solid #FF0000;
         -webkit-border-radius: 100px;
         height: 100px;
-        width: 100px;		
+        width: 100px;
         -webkit-animation: pulsate 1s ease-out;
-        -webkit-animation-iteration-count: infinite; 
+        -webkit-animation-iteration-count: infinite;
         /*opacity: 0.0*/
     }
-
+    
     @-webkit-keyframes pulsate {
-            0% {-webkit-transform: scale(0.1, 0.1); opacity: 0.0;}
-            50% {opacity: 1.0;}
-            100% {opacity: 1.0;}
+        0% {
+            -webkit-transform: scale(0.1, 0.1);
+            opacity: 0.0;
+        }
+        50% {
+            opacity: 1.0;
+        }
+        100% {
+            opacity: 1.0;
+        }
     }
+    
     .parameter {
         display: flex;
-   }
+    }
 </style>
-@endsection
-@endsection
-
-@section('content')
+@endsection @endsection @section('content')
 <br>
 <div class="row">
-	<div class="container">
-		<div class="col-md-12">
-			<ol class="breadcrumb">
-				<li class="breadcrumb-item"><a href="/" class="text-primary">Home</a></li>
-				<li class="breadcrumb-item active"><a href="/gempa" class="text-primary" >Gempabumi</a></li>
-			</ol>
-		</div>
-	</div>
+    <div class="container">
+        <div class="col-md-12">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/" class="text-primary">Home</a></li>
+                <li class="breadcrumb-item active"><a href="/gempa" class="text-primary">Gempabumi</a></li>
+            </ol>
+        </div>
+    </div>
 </div>
 <br>
+<!-- this is eq map -->
 <div class="row">
-	<div class="container">
-		<div class="col-md-12 col-lg-12">
+    <div class="container">
+        <div class="col-md-12 col-lg-12">
             <div id="streetmap">
-                <img src=" {{ asset('images') }}/logo.jpg " alt="logo" width="100%" height="20%" style="margin-bottom: 15px" > 
-                    <div id="map" style="width:85%;height:500px;"></div> 
+                <img src=" {{ asset('images') }}/logo.jpg " alt="logo" width="100%" height="20%" style="margin-bottom: 15px">
+                <div id="map" style="width:85%;height:500px; border-radius: 5px;"></div>
             </div>
-	    </div>
-	</div>
+        </div>
+    </div>
+</div>
+<br>
+<!-- eq parameter -->
+<div class="row">
+    <div class="container parameter">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <img src="http://inatews.bmkg.go.id/light/images/clock.png" alt="waktu gempa" aria-hidden="true"> Waktu Gempa
+                </div>
+                <div class="card-body">
+                    <p class="card-text">{{ $event['tanggal'] }} {{ $event['origin'] }} UTC</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header align-middle">
+                    <i class="fa fa-map-marker text-danger" style="font-size: 2.5em;" aria-hidden="true"></i> Lokasi
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Lokasi: {{ $event['lintang'] }} , {{ $event['bujur'] }} BT</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://inatews.bmkg.go.id/light/images/mag.png" alt="Magnitude" aria-hidden="true"> Kekuatan
+                </div>
+                <div class="card-body">
+                    <p class="card-text">M <span class="badge badge-primary">{{ $event['magnitudo'] }}</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <br>
 <div class="row">
-	<div class="container parameter" >
-		<div class="col-md-4">
-			<div class="card">
-			  <div class="card-header">
-			  	<img src="http://inatews.bmkg.go.id/light/images/clock.png" alt="waktu gempa" aria-hidden="true">
-			    Waktu Gempa
-			  </div>
-			  <div class="card-body">
-			    <p class="card-text">{{ $event['tanggal'] }} {{ $event['origin'] }} UTC</p>
-			  </div>
-			</div>
-		</div>
-		<div class="col-md-4">
-			<div class="card">
-			  <div class="card-header">
-			  	<i class="fa fa-map-marker text-danger" style="font-size: 2.5em;" aria-hidden="true" ></i>
-			    Lokasi
-			  </div>
-			  <div class="card-body">
-			    <p class="card-text">Lokasi: {{ $event['lintang'] }} , {{ $event['bujur'] }}</p>
-			  </div>
-			</div>
-		</div>
+    <div class="container parameter">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <img src="http://inatews.bmkg.go.id/light/images/kedalaman.png" alt="depth" aria-hidden="true"> Kedalaman
+                </div>
+                <div class="card-body">
+                    <p class="card-text">{{ $event['depth'] }} Km</p>
+                </div>
+            </div>
+        </div>
 
-		<div class="col-md-4">
-			<div class="card">
-			  <div class="card-header">
-			  	<img src="https://inatews.bmkg.go.id/light/images/mag.png" alt="Magnitude" aria-hidden="true">
-			    Kekuatan
-			  </div>
-			  <div class="card-body">
-			    <p class="card-text">M <span class="badge badge-primary">{{ $event['magnitudo'] }}</span></p>
-			  </div>
-			</div>
-		</div>
-	</div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header align-middle">
+                    <i class="fa fa-file-text-o text-primary" aria-hidden="true" style="font-size: 2.5em;"></i> Keterangan
+                </div>
+                <div class="card-body">
+                    <p class="card-text">{{ $event['ket'] }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header align-middle">
+                    <i class="wi wi-earthquake text-danger" aria-hidden="true" style="font-size: 2.5em;"></i> Dirasakan
+                </div>
+                <div class="card-body">
+                    <p class="card-text">{{ $event['terdampak'] or ' '}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<br>
-<div class="row">
-	<div class="container parameter">
-		<div class="col-md-4">
-			<div class="card">
-			  <div class="card-header">
-			  	<img src="http://inatews.bmkg.go.id/light/images/kedalaman.png" alt="depth" aria-hidden="true">
-			    Kedalaman
-			  </div>
-			  <div class="card-body">
-			    <p class="card-text">{{ $event['depth'] }} Km</p>
-			  </div>
-			</div>
-		</div>
+<br> @endsection @section('script')
+<script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js" integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA==" crossorigin=""></script>
+<script src="{{ asset('gjson') }}/patahan.js">
+</script>
+<script src="{{ asset('gjson') }}/subduksi.js">
+</script>
+<script src="{{ asset('gjson') }}/plates.js">
+</script>
 
-		<div class="col-md-4">
-			<div class="card">
-			  <div class="card-header">
-			  	<i class="fa fa-file-text-o text-primary" aria-hidden="true" style="font-size: 2.5em;" ></i> 
-			    Keterangan
-			  </div>
-			  <div class="card-body">
-			    <p class="card-text">{{ $event['ket'] }}</p>
-			  </div>
-			</div>
-		</div>
+<script>
+    var mymap = L.map('map').setView([ {{$event['lintang']}}, {{ $event['bujur']}}], 7);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        id: 'mapbox.satellite'
+    }).addTo(mymap);
 
-		<div class="col-md-4">
-			<div class="card">
-			  <div class="card-header">
-			  	<i class="wi wi-earthquake text-danger" aria-hidden="true" style="font-size: 2.5em;" ></i>
-			    Dirasakan
-			  </div>
-			  <div class="card-body">
-			    <p class="card-text">{{ $event['terdampak'] or ' '}}</p>
-			  </div>
-			</div>
-		</div>
-	</div>
-</div>
-<br>
-@endsection
-@section('script')
-    <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
-   integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
-   crossorigin=""></script>
-   <script src="{{ asset('gjson') }}/patahan.js" > </script>
-   <script src="{{ asset('gjson') }}/subduksi.js" > </script>
-   <script src="{{ asset('gjson') }}/plates.js" > </script>
+    var eqIcon = L.icon({
+        iconUrl: '/images/icongempa.png',
 
-    <script>
-        var mymap = L.map('map').setView([{{ $event['lintang'] }}, {{ $event['bujur'] }}], 7);
+        iconSize: [40, 40], // size of the icon
+    });
+    L.marker([{{ $event['lintang'] }}, {{ $event['bujur'] }}], {
+        icon: eqIcon
+    }).addTo(mymap);
+    //style for subduksi and patahan
+    var patahanStyle = {
+        "color": "#2C4B48",
+        "weight": 0.7,
+        "opacity": 0.9
+    };
 
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            maxZoom: 18,
-            id: 'mapbox.streets'
-        }).addTo(mymap);
+    //style for subduksi
 
-        var eqIcon = L.icon({
-            iconUrl: '/images/icongempa.png',
+    var subduksiStyle = {
+        "color": "#0A70C4",
+        "weight": 1,
+        "opacity": 0.5,
+        "fillColor": 'transparent',
+    }
 
-            iconSize:     [40, 40], // size of the icon
-        });
-        L.marker([ {{$event['lintang'] }}, {{ $event['bujur'] }}], {icon: eqIcon}).addTo(mymap);
-        //style for subduksi and patahan
-        var patahanStyle = {
-            "color": "#2C4B48",
-            "weight": 0.7,
-            "opacity": 0.9
-        };
-
-        //style for subduksi
-
-        var subduksiStyle = {
-            "color": "#0A70C4",
-            "weight": 1,
-            "opacity": 0.5,
-            "fillColor": 'transparent',
-        }
-
-        function onEachFeature(feature, layer) {
+    function onEachFeature(feature, layer) {
         // does this feature have a property named popupContent?
-            if (feature.properties && feature.properties.PlateName) {
-                layer.bindPopup(feature.properties.PlateName);
-            }
+        if (feature.properties && feature.properties.PlateName) {
+            layer.bindPopup(feature.properties.PlateName);
         }
+    }
 
-        L.geoJSON(pataHan, {
-            style : patahanStyle,
-        }).addTo(mymap); //add patahan symbol
+    L.geoJSON(pataHan, {
+        style: patahanStyle,
+    }).addTo(mymap); //add patahan symbol
 
-        // L.geoJSON(worldPlates, {
-        //     style: subduksiStyle,
-        //     onEachFeature: onEachFeature
-        // }).addTo(mymap);
+    //plot subduction
+    L.geoJSON(subDuksi, {
+        style: subduksiStyle
+    }).addTo(mymap);
 
-        //plot subduction
-        L.geoJSON(subDuksi, {
-            style: subduksiStyle
-        }).addTo(mymap);
+    // Define an icon called cssIcon
+    var cssIcon = L.divIcon({
+        // Specify a class name we can refer to in CSS.
+        className: 'css-icon',
+        html: '<div class="gps_ring"></div>'
+            // Set marker width and height
+            ,
+        iconSize: [100, 100]
+            // ,iconAnchor: [11,11]
+    });
 
-		// Define an icon called cssIcon
-		var cssIcon = L.divIcon({
-		  // Specify a class name we can refer to in CSS.
-		  className: 'css-icon',
-		  html: '<div class="gps_ring"></div>'
-		  // Set marker width and height
-		  ,iconSize: [100,100]
-		  // ,iconAnchor: [11,11]
-		});
-    
     //add css icon as gps ring
-    L.marker([ {{$event['lintang'] }}, {{ $event['bujur'] }} ], {icon: cssIcon} ).addTo(mymap);
-    </script>
+    L.marker([{{ $event['lintang'] }}, {{ $event['bujur'] }}], {
+        icon: cssIcon
+    }).addTo(mymap);
+</script>
 @endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,17 +3,19 @@
 <html lang="en">
    <head>
       <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <meta name="description" content="Stasiun Geofisika Angkasapura">
       <meta name="keywords" content="Gempabumi, Magnetbumi, Listrik Udara, Percepatan Tanah">
       <meta name="author" content="Jambari">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
       @yield('title')
       <link rel="stylesheet" href="{{ asset('css') }}/bootstrap.min.css">
       <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet"> 
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="shortcut icon" href="https://bmkg.go.id/asset/img/favicon.ico" type="image/x-icon">
       <link rel="stylesheet" href="{{ asset('css/weather-icons.min.css') }}">
+      <link href="https://cdn.rawgit.com/sachinchoolur/lightgallery.js/master/dist/css/lightgallery.css" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href=" {{ asset('css/demogallery.css') }} ">
       <style>
          #stasiun {
          font-family: 'Karla', sans-serif;
@@ -37,55 +39,9 @@
    </head>
    <body>
       @yield('alert')
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-         <a class="navbar-brand" href="/">
-         <img src="{{ asset('images') }}/logo-bmkg.png" alt="logo" width="35" height="40" >
-         </a>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-         <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-               <li class="nav-item active">
-                  <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-               </li>
-               <li class="nav-item active">
-                  <a class="nav-link" href="/news">Berita</a>
-               </li>
-               <li class="nav-item active">
-                  <a class="nav-link" href="/siarans">Siaran Press</a>
-               </li>
-               <li class="nav-item active">
-                  <a class="nav-link" href="/gempa">Gempabumi</a>
-               </li>
-               {{--           
-               <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <a class="dropdown-item" href="#">Action</a>
-                     <a class="dropdown-item" href="#">Another action</a>
-                     <div class="dropdown-divider"></div>
-                     <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link disabled" href="#">Disabled</a>
-               </li>
-               --}}
-            </ul>
-            {{--    
-            <form class="form-inline my-2 my-lg-0">
-               <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            --}}
-         </div>
-      </nav>
+      @include('nav')
       @yield('content')
       @include('footer')
-
       @yield('script')
    </body>
 </html>
