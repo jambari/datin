8f3f0d934ea6bc5aa10eee9a50139cdb411d5f9a
jambari <jambari07@gmail.com>
2019-03-26 06:49:42 +0000

    change body padding to px unit

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -22,7 +22,7 @@
       <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>
       <style>
         body {
-          padding-top: 5.5%;
+          padding-top: 75px;
         }
          #stasiun {
          font-family: 'Karla', sans-serif;
