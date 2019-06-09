ab63496ab79172cc40804124daba2f2caf8b3eee
jambari <jambari07@gmail.com>
2019-06-09 10:04:51 +0900

    ubah waktu from wib to wit

--- app/Models/Balaisms.php
+++ app/Models/Balaisms.php
@@ -51,14 +51,27 @@ class Balaisms extends Model
         $lintang = $this->attributes['lintang'];
         $bujur = $this->attributes['bujur'];
         if ($bujur >= 132 and $bujur <= 141 and $lintang >= -7.5 and $lintang <= 5) {
-            // if () {
                 $value = "PGR V";
                 return $value;
-            // }
         } else {
             $value = "BUKAN PGR V";
             return $value;
         }
+    }
+
+    //accesor sms
+    public function getSmsAttribute ($value) {
+        $regex = '/(\d{2})-\w[A-z][a-z]-(\d{2}) (\d{2}):(\d{2}):(\d{2})/';
+        preg_match($regex, $value, $result);
+        $tt = implode(' ',$result);
+        //create datetime on WIB timezone Asia/Jakarta
+        $oldtime = $tt[0].$tt[1].'-'.$tt[3].$tt[4].$tt[5].'-'.$tt[7].$tt[8].' '.$tt[10]
+        .$tt[11].':'.$tt[13].$tt[14].':'.$tt[16].$tt[17];
+        $new_time = strtotime($oldtime);
+        $new_time = strtotime("+2 hours", $new_time);
+        $new_time = date("d-M-y H:i:s", $new_time);
+        $value = str_replace($oldtime, $new_time, $value);
+        return $value;
     }
     /*
     |--------------------------------------------------------------------------
