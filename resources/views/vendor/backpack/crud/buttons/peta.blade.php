793967c4cc0e826fe0153fb3cb49c4d908f51d11
jambari <jambari07@gmail.com>
2019-06-09 14:13:12 +0900

    change infogempa balai 5 design

--- resources/views/vendor/backpack/crud/buttons/peta.blade.php
+++ resources/views/vendor/backpack/crud/buttons/peta.blade.php
@@ -1,3 +1,3 @@
 @if ($crud->hasAccess('peta'))
-	<a href="{{ url($crud->route.'/'.$entry->getKey().'/peta') }}" class="btn btn-xs btn-default"><i class="fa fa-download"></i> Peta</a>
+	<a href="{{ url($crud->route.'/'.$entry->getKey().'/peta') }}" class="btn btn-xs btn-success"><i class="fa fa-map"></i> Peta</a>
 @endif
--- resources/views/vendor/backpack/crud/buttons/update.blade.php
+++ resources/views/vendor/backpack/crud/buttons/update.blade.php
@@ -2,7 +2,7 @@
 	@if (!$crud->model->translationEnabled())
 
 	<!-- Single edit button -->
-	<a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}" class="btn btn-xs btn-default"><i class="fa fa-edit"></i> {{ trans('backpack::crud.edit') }}</a>
+	<a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}" class="btn btn-xs btn-warning"><i class="fa fa-edit"></i> {{ trans('backpack::crud.edit') }}</a>
 
 	@else
 
