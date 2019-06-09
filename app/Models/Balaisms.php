b53c318a4fbbf24e25acaf47fd256dcae4b06d49
jambari <jambari07@gmail.com>
2019-06-09 10:50:07 +0000

    perbaiki error di zona waktu

--- app/Models/Balaisms.php
+++ app/Models/Balaisms.php
@@ -61,16 +61,15 @@ class Balaisms extends Model
 
     //accesor sms
     public function getSmsAttribute ($value) {
-        $regex = '/(\d{2})-\w[A-z][a-z]-(\d{2}) (\d{2}):(\d{2}):(\d{2})/';
-        preg_match($regex, $value, $result);
-        $tt = implode(' ',$result);
-        //create datetime on WIB timezone Asia/Jakarta
-        $oldtime = $tt[0].$tt[1].'-'.$tt[3].$tt[4].$tt[5].'-'.$tt[7].$tt[8].' '.$tt[10]
-        .$tt[11].':'.$tt[13].$tt[14].':'.$tt[16].$tt[17];
-        $new_time = strtotime($oldtime);
-        $new_time = strtotime("+2 hours", $new_time);
-        $new_time = date("d-M-y H:i:s", $new_time);
-        $value = str_replace($oldtime, $new_time, $value);
+        // $regex = '/(\d{2})-\w[A-z][a-z]-(\d{2}) (\d{2}):(\d{2}):(\d{2})/';
+        // preg_match($regex, $value, $result);
+        // $tt = implode(' ',$result);
+        // $oldtime = $tt[0].$tt[1].'-'.$tt[3].$tt[4].$tt[5].'-'.$tt[7].$tt[8].' '.$tt[10]
+        // .$tt[11].':'.$tt[13].$tt[14].':'.$tt[16].$tt[17];
+        // $new_time = strtotime($oldtime);
+        // $new_time = strtotime("+2 hours", $new_time);
+        // $new_time = date("d-M-y H:i:s", $new_time);
+        // $value = str_replace($oldtime, $new_time, $value);
         return $value;
     }
     /*
