87c4544a46ccc43aaf10abcc1b22b529cbc22691
jambari <jambari07@gmail.com>
2019-10-11 14:17:51 +0900

    change layour detail gempa

--- app/Http/Controllers/Admin/GempaCrudController.php
+++ app/Http/Controllers/Admin/GempaCrudController.php
@@ -313,10 +313,63 @@ class GempaCrudController extends CrudController
     //detail eq on frontend
     public function showmap($id) {
         $event = $this->crud->getEntry($id);
-        return view('gempa.detail_gempa', compact('event'));
+        //Penanggalan
+        //array bulan
+            $bulan = array (
+            1 =>   'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
+        );
+        //array hari senin-sabtu
+        $days = array (
+            0 =>   'Minggu',
+            'Senin',
+            'Selasa',
+            'Rabu',
+            'Kamis',
+            "Jum'at",
+            'Sabtu'
+    );
+        $tanggal = $event['tanggal']; //get date of the eathquake
+        $jam = $event['origin']; // get origin time of eq
+        $tanggaljam = $tanggal." ".$jam; //susun tanggal dari kolom tanggal dan origin
+        $tanggalbaru = date("d-m-Y", strtotime($tanggaljam)); //mengubah ke tipe datetime
+        $hari = (int)date("w", strtotime($tanggaljam)); //ambil angka hari dalam sebuah minggu
+        $hari = $days[$hari];
+        $pecahkan = explode('-',$tanggalbaru);
+        $tanggalindo = $pecahkan[0] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[2]; //Menggabungkan jadi tanggal format indonesia
+        $jamutc = date("d-m-Y H:i:s", strtotime($tanggaljam)); //mengubah ke tipe datetime
+        $jamwit = date("H:i:s", strtotime($jamutc) + 32400);
+        $jamsusulan = date("H:i", strtotime($jamutc) + 34200);
+        $lat = $event['lintang'];
+        $lon = $event['bujur'].' BT';
+        $mag = round($event['magnitudo'],1);
+        $depth = $event['depth'];
+        //wilayah yang diguncang gempa
+        $wilayah = $event['ket'];
+        $ket = explode(" ", $wilayah);
+        $wilayah = $ket[3];
+        $arah = $ket[2];
+        $jarak = $ket[0];
+        $lat = str_split($lat); //break latitude to an array
+        if ($lat[0] == '-') {
+            $lat = $lat[1].$lat[2].$lat[3].$lat[4].' LS';
+        } else {
+            $lat = $lat[1].$lat[2].'LU';
+        }
+        return view('gempa.detail_gempa', compact('lat', 'lon', 'mag','wilayah', 'depth','event', 'arah', 'jarak', 'tanggalindo', 'hari', 'jamwit','event'));
     }
     //recent eq statistik
-    public function press($id)
+    public function press($id) //rilis media
     {
         $event = Gempa::find($id);
         //Penanggalan
