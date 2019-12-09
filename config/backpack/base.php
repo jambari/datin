268423f32e5de2845b8034d1904b13e984b31bc4
jambari <jambari07@gmail.com>
2019-12-09 09:00:22 +0900

    set registration to false

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -25,10 +25,10 @@ return [
     'developer_link' => 'http://github.com/jambari',
 
     // Show powered by Laravel Backpack in the footer?
-    'show_powered_by' => true,
+    'show_powered_by' => false,
 
     // The AdminLTE skin. Affects menu color and primary/secondary colors used throughout the application.
-    'skin' => 'skin-green-light',
+    'skin' => 'skin-purple',
     // Options: skin-black, skin-blue, skin-purple, skin-red, skin-yellow, skin-green, skin-blue-light, skin-black-light, skin-purple-light, skin-green-light, skin-red-light, skin-yellow-light
 
     // Date & Datetime Format Syntax: https://github.com/jenssegers/date#usage
@@ -48,7 +48,7 @@ return [
     | By default the registration is open only on localhost.
     */
 
-    'registration_open' => true,
+    'registration_open' => false,
 
     /*
     |--------------------------------------------------------------------------
