68645f2ec4346da636ad3a1e4439c11fa75a4d80
jambari <jambari07@gmail.com>
2022-08-24 16:56:08 +0900

    delete delete and edit button for guest

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -26,7 +26,7 @@
                 <div class="row" style="margin-top: 3%;">
                                     @if ($guests)
                     @foreach ($guests as $guest)
-                    <a href="{{ route('guests.show',$guest->id) }}">
+<!--                     <a href="{{ route('guests.show',$guest->id) }}"> -->
                     <div class="col-md-4" style="margin-bottom: 3%;">
                         
                     <div class="card" style="width: 18rem;">
@@ -36,14 +36,14 @@
                         <h5 class="card-text text-dark">{{ $guest->dari }} </h5>
                         <p class="card-text text-dark">{{ $guest->keperluan }}</p>
                         <p class="card-text text-info">{{ \Carbon\Carbon::parse($guest->created_at)->format('d-M-Y')}}</p>
-                        <form action="{{ route('guests.destroy',$guest->id) }}" method="POST">
+<!--                         <form action="{{ route('guests.destroy',$guest->id) }}" method="POST">
                         <a class="btn btn-primary" href="{{ route('guests.edit',$guest->id) }}">Edit</a>
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Delete</button>
+                        <button type="submit" class="btn btn-danger">Delete</button> -->
                       </div>
                     </div>
-                    </a>
+<!--                     </a> -->
                     </div>
                 @endforeach
                     {!! $guests->links() !!}
--- resources/views/guests/show.blade.php
+++ resources/views/guests/show.blade.php
@@ -1,7 +1,7 @@
 @extends('guests.layout')
 
 @section('content')
-
+<img class="card-img-top" src="{{ \Storage::url('photos/' . $guest->foto) }}" alt="Card image cap">
 <p> nama: {{ $guest->nama }} </p>
 <p>Dari :{{ $guest->dari }} </p>
 @endsection
